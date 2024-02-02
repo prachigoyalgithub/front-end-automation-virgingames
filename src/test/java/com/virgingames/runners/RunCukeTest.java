4018abbb26d83e2535f98912df2cf589bea396a6
prachigoyalgithub <prachigoyal.new@gmail.com>
2024-02-02 20:26:42 +0000

    front-end-automation-virgingames

---
+++ src/test/java/com/virgingames/runners/RunCukeTest.java
@@ -0,0 +1,18 @@
+package com.virgingames.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/virgingames/steps",
+        plugin = {"html:target/cucumber-reports/cucumber.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "json:target/cucumber-reports/cucumber.json"},
+        tags = ""
+)
+
+public class RunCukeTest extends AbstractTestNGCucumberTests {
+
+}
